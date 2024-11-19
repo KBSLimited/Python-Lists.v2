afd5b4ff4331741101d0cb30727a5b4050ad8236
KBSLimited <jdeandre1220@gmail.com>
2024-11-19 07:11:01 -0800

    Grades averaged

--- 1. Python List Transformation.py
+++ 1. Python List Transformation.py
@@ -2,3 +2,8 @@
 grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
 sorted_grades = sorted(grades, reverse=True)
 print(sorted_grades)
+
+#Task 2: Calculate the average grade and print it.
+grades = [95, 93, 90, 89, 88, 85, 80, 78, 76, 72]
+average_grade = sum(grades) / len(grades)
+print(f"Average grade: {average_grade:.2f}")
